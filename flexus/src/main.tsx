432e18bab45ab2ee54c5c50b48ab3de4f40d3db0
서동균 <sudong111@naver.com>
2024-08-16 16:26:22 +0900

    lostArk api 연동 테스트

--- flexus/src/main.tsx
+++ flexus/src/main.tsx
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
+import instance from "./instance";
 
 function Main() {
+    const getAPIData = async () => {
+        const res = instance({
+            method: "get",
+            url: "news/notices"
+        }).then((data) => {
+            console.log(data);
+        });
+    };
+
+    useEffect(() => {
+        getAPIData();
+    }, []);
+
     return (
         <div className="main">
             <header className="bg-blue-600 flex flex-wrap text-nowrap justify-between items-center py-4 px-10 min-w-[850px] ">
