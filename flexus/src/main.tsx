f10ff81c21c62e47753c9436cf578601c69ea08e
서동균 <sudong111@naver.com>
2024-08-16 15:19:17 +0900

    작업 시작

---
+++ flexus/src/main.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Main() {
+    return (
+        <div className="main">
+            <header className="bg-blue-600 flex flex-wrap text-nowrap justify-between items-center py-4 px-10 min-w-[850px] ">
+                <div>
+                    <p className="text-white">Flexus 길드 혈석 계산기</p>
+                </div>
+            </header>
+        </div>
+    )
+}
+
+export default Main;
