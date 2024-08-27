654536bdf9c64b43ec942fd673a51a7f14f8057a
서동균 <sudong9409@gmail.com>
2024-08-27 17:22:24 +0900

    tailwind css 정리 card 추가

--- flexus/src/pages/main.tsx
+++ flexus/src/pages/main.tsx
@@ -1,11 +1,21 @@
 import React, {useEffect, useState} from 'react';
 import { Link, Route } from 'react-router-dom';
+import ApexCharts from 'react-apexcharts';
 
 export default function Main() {
 
     return (
-        <div className="w-full p-5 bg-white dark:bg-gray-900 antialiased !p-0">
-test
+        <div className="div-css">
+            <div className="div-30-css max-h-96">
+                <div className="card-css">
+                    <h5 className="title-css pb-3">오늘 할 일</h5>
+                    <p className="text-gray-600">10:00 - 아침 운동</p>
+                </div>
+                <div className="card-css">
+                    <h5 className="title-css pb-3">오늘 할 일</h5>
+                    <p className="text-gray-600">10:00 - 아침 운동</p>
+                </div>
+            </div>
         </div>
     )
 }
