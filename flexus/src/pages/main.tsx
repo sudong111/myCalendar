74d8e672182ee8a0a4fe1f95e14301a534422dec
서동균 <sudong111@naver.com>
2024-08-28 23:44:05 +0900

    캘린더 해더 추가

--- flexus/src/pages/main.tsx
+++ flexus/src/pages/main.tsx
@@ -1,21 +1,13 @@
-import React, {useEffect, useState} from 'react';
-import { Link, Route } from 'react-router-dom';
-import ApexCharts from 'react-apexcharts';
+import Calendar from '../components/calendar';
 
 export default function Main() {
 
     return (
         <div className="div-css">
-            <div className="div-30-css max-h-96">
-                <div className="card-css">
-                    <h5 className="title-css pb-3">오늘 할 일</h5>
-                    <p className="text-gray-600">10:00 - 아침 운동</p>
-                </div>
-                <div className="card-css">
-                    <h5 className="title-css pb-3">오늘 할 일</h5>
-                    <p className="text-gray-600">10:00 - 아침 운동</p>
-                </div>
+            <div className="">
+                <Calendar/>
             </div>
+
         </div>
     )
 }
