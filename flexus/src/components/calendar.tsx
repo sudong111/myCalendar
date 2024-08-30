f4db6957f2ab4d7b4efbac8137bd764969b4fe93
서동균 <sudong9409@gmail.com>
2024-08-31 07:15:58 +0900

    공휴일 달력에 표시 및 css 수정

--- flexus/src/components/calendar.tsx
+++ flexus/src/components/calendar.tsx
@@ -1,28 +1,20 @@
 import React, {useEffect, useState } from 'react';
 import CalendarHeader from './calendar/header';
 import CalendarWeek from './calendar/week';
-import CalendarDays from './calendar/days'
+import CalendarDays from './calendar/days';
+import {holidayDto} from '../Dto/calendar.dto';
 import axios from 'axios';
 
 export default function Calendar() {
 
     const [currentMonth, setCurrentMonth] = useState(new Date());
-    const [anniversary, setAnniversary] = useState([]);
-    const [holidays, setHolidays] = useState([]);
-    
+    const [holiday, setHoliday] = useState<holidayDto[]>([]);
+    const [dataLoaded, setDataLoaded] = useState(false);
+
     const fetchData = async () => {
         try {
-            // 두 API 호출을 병렬로 실행합니다
-            const [anniversaryRes, holidaysRes] = await Promise.all([
-                axios.get('https://apis.data.go.kr/B090041/openapi/service/SpcdeInfoService/getAnniversaryInfo', {
-                    params: {
-                        ServiceKey: process.env.REACT_APP_DATA_GO_KR_API_KEY,
-                        pageNo: 1,
-                        nomOfRows: 100,
-                        solYear: currentMonth.getFullYear(),
-                        solMonth: ("0" + (currentMonth.getMonth() + 1)).slice(-2)
-                    }
-                }),
+            let holidayList = [];
+            const [holidaysRes] = await Promise.all([
                 axios.get('https://apis.data.go.kr/B090041/openapi/service/SpcdeInfoService/getRestDeInfo', {
                     params: {
                         ServiceKey: process.env.REACT_APP_DATA_GO_KR_API_KEY,
@@ -31,15 +23,27 @@ export default function Calendar() {
                     }
                 })
             ]);
-
-            setAnniversary(anniversaryRes.data.response.body.items);
-            setHolidays(holidaysRes.data.response.body.items);
+            const holidayData = holidaysRes.data.response.body.items.item;
+            
+            if(holidayData != undefined) {
+                if(!Array.isArray(holidayData)) {
+                    holidayList.push(holidayData);
+                }
+                else {
+                    holidayList = holidayData;
+                }
+            }
+            
+            setHoliday(holidayList || []);
         } catch (error) {
             console.error('Error fetching data:', error);
+        } finally {
+            setDataLoaded(true);
         }
     };
 
     useEffect(() => {
+        setDataLoaded(false);
         fetchData();
     }, [currentMonth]);
 
@@ -55,13 +59,14 @@ export default function Calendar() {
                changedMonth={handleChangeMonth}
            />
             <CalendarWeek />
+            {dataLoaded && (
             <CalendarDays
                 dateParams={
                 {
                     month: currentMonth,
-                    holidays: holidays,
-                    anniversary: anniversary
+                    specialDay: holiday
                 }}/>
+            )}
         </div>
     )
 }
